892951c3a23a08d91071187384cd4ecf12056e08
dhiman saha <partho.saha01@gmail.com>
2023-06-08 18:29:44 +0600

    ''multipe-insertion-done'

--- routeHandler/todoHandler.js
+++ routeHandler/todoHandler.js
@@ -28,7 +28,17 @@ router.post('/', async(req, res)=>{
 })
 
 router.post('/all', async(req, res)=>{
-    
+    try{
+      await Todo.insertMany(req.body);
+      res.status(200).json({
+        Message: "TOdos were inserted",
+      })
+    }
+    catch(err){
+      res.status(500).json({
+        error: "Server Error"+ err,
+      })
+    }
 })
 
 router.put('/:id', async(req, res)=>{
